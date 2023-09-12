1c955354311c959a95db539da36c272b7614b658
kangmina91 <133613566+kangmina91@users.noreply.github.com>
2023-09-12 15:18:27 +0900

    0912
    0912

--- js/custom.js
+++ js/custom.js
@@ -33,16 +33,16 @@ new fullpage('#main', {
     // Demo 페이지 구조가 생성된 직후에 이 콜백이 실행
     afterRender: function () {
         // INTRO.style.backgroundColor = "black";
-        INTRO.style.zIndex = 2008;
+        INTRO.style.zIndex = 9999;
         UFO();
         setTimeout(function () {
-
-
-            BOX_COVER.classList.add('on');
-
+            console.log(1);
             setTimeout(function () {
-
-
+                console.log(2);
+                setTimeout(function () {
+                    console.log(3);
+                    BOX_COVER.classList.add('on');
+                }, 1000);
                 INTRO.style.zIndex = 999;
                 CHARS_FALL();
                 SLOGAN.classList.add('on');
